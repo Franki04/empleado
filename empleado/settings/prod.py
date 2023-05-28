86d4b4ea0c41310a56ca8c903d6466739dd99a11
Franki04 <fran_vera23@hotmail.es>
2023-05-28 13:44:04 +0200

    archivo prod

---
+++ empleado/settings/prod.py
@@ -0,0 +1,38 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbempleado',
+        'USER': 'fran',
+        'PASSWORD': 'fran',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static'] # Carpeta base static (constante que indica a Django que tenemos una carpeta static donde tendremos ubicados nuestros archivos estaticos)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
