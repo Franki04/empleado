d88d0b438222ccc3b7ff69a9800933202c8e45af
Franki04 <fran_vera23@hotmail.es>
2023-05-28 20:24:27 +0200

    ALLOWED_HOSTS = []

--- empleado/settings/prod.py
+++ empleado/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.241.131.148']
+ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
