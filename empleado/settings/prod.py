747ecc292b64934195092fb556b7b3441a617ec6
Franki04 <111075223+Franki04@users.noreply.github.com>
2023-05-28 22:13:08 +0200

    Update prod.py

--- empleado/settings/prod.py
+++ empleado/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.241.131.148']
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -33,6 +33,7 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static'] # Carpeta base static (constante que indica a Django que tenemos una carpeta static donde tendremos ubicados nuestros archivos estaticos)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
